de3cf11b8b12df5429f6a7cc724f041bc4b238a9
sahin <sahin@DESKTOP-1SCT98J>
2023-07-26 17:28:32 +0300

    Add project files.

---
+++ Casgem_Mediator/Controllers/DefaultController.cs
@@ -0,0 +1,53 @@
+﻿using Casgem_Mediator.MediatorPattern.Commands;
+using Casgem_Mediator.MediatorPattern.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Casgem_Mediator.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly IMediator _meditor;
+        public DefaultController(IMediator meditor)
+        {
+            _meditor = meditor;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var values = await _meditor.Send(new GetProductQuery());
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(CreateProductCommand command)
+        {
+            await _meditor.Send(command);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            await _meditor.Send(new RemoveProductCommand(id));
+            return RedirectToAction("Index");
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(int id)
+        {
+            var value = await _meditor.Send(new GetProductUpdateByIDQuery(id));
+            return View(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductCommand command)
+        {
+            var value = await _meditor.Send(command);
+            return RedirectToAction("Index");
+        }
+    }
+}
